5be842e1fbfba513dd28b6b9a5bd9f1cf7859b3d
Cat Whitmer <60889037+catwhitmer@users.noreply.github.com>
2022-10-20 11:05:30 -0400

    Updated Not found page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,6 +1,22 @@
+import { Link } from "react-router-dom"
+import HomeIcon from '@mui/icons-material/Home';
+
 function NotFound() {
   return (
-    <div>NotFound</div>
+    <div className="hero">
+      <div className="max-w-lg">
+        <h1 className="text-8xl font-bold mb-8">
+          Oops!
+        </h1>
+        <p className="text-5xl mb-8">
+          404 - Page Not Found!
+        </p>
+        <Link to='/' className="btn btn-primary btn-lg" >
+          <HomeIcon className="mr-2" />
+          Home
+        </Link>
+      </div>
+    </div>
   )
 }
 
