646ea6189db93a54e222873677ba40ffdf65586b
Cat Whitmer <60889037+catwhitmer@users.noreply.github.com>
2022-10-20 10:26:51 -0400

    Added footer

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Route} from 'react-router-dom'
+import Footer from './components/Footer';
 import NavBar from './components/NavBar'
 
 function App() {
@@ -6,7 +7,8 @@ function App() {
     <Router>
       <div className='flex flex-col justify-between h-screen'>
         <NavBar />
-        <main>Content</main>
+        <main className='container mx auto px-3 pb-12'>Content</main>
+        <Footer />
       </div>  
     </Router>
   )
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -12,7 +12,6 @@ function NavBar({ title }) {
             {title}
           </Link>
         </div>
-
         <div className='flex-1 px-2 mx-2'>
           <div className='flex justify-end'>
             <Link to='/' className='btn btn-ghost btn-sm rounded-btn'>
