5d7d7055f34368949a45cff31ab342e1571f6d08
Cat Whitmer <60889037+catwhitmer@users.noreply.github.com>
2022-10-28 17:41:07 -0400

    Restyled Repo

--- src/components/repos/RepoItem.jsx
+++ src/components/repos/RepoItem.jsx
@@ -1,4 +1,5 @@
 import PropTypes from "prop-types"
+
 import LinkIcon from '@mui/icons-material/Link'
 import VisibilityIcon from '@mui/icons-material/Visibility'
 import StarBorderPurple500Icon from '@mui/icons-material/StarBorderPurple500'
@@ -13,35 +14,30 @@ function RepoItem({ repo }) {
     forks,
     open_issues,
     watchers_count,
-    stargazerz_count
+    stargazers_count
   } = repo
 
   return (
-    <div className="mb-2 rounded-md card bg-gray-800 hover:bg-gray-900">
-      <div className="card-body">
-        <h3 className="mb-2 text-xl font-semibold">
+    <div className='mb-2 rounded-md card bg-base-200 hover:bg-base-300'>
+      <div className='card-body'>
+        <h3 className='mb-2 text-xl font-semibold'>
           <a href={html_url}>
-            {<LinkIcon className="inline mr-1"/>}
-            {name}
+            <LinkIcon className='inline mr-1' /> {name}
           </a>
         </h3>
-        <p className="mb-3">{description}</p>
+        <p className='mb-3'>{description}</p>
         <div>
-          <div className="mr-2 badge-info badge-lg">
-            {<VisibilityIcon className="mr-2" />}
-            {watchers_count}
+          <div className='mr-2 badge badge-info badge-lg'>
+            <VisibilityIcon className='mr-2' /> {watchers_count}
           </div>
-          <div className="mr-2 badge-success badge-lg">
-            {<StarBorderPurple500Icon className="mr-2" />}
-            {stargazerz_count}
+          <div className='mr-2 badge badge-success badge-lg'>
+            <StarBorderPurple500Icon className='mr-2' /> {stargazers_count}
           </div>
-          <div className="mr-2 badge-error badge-lg">
-            {<InfoIcon className="mr-2" />}
-            {open_issues}
+          <div className='mr-2 badge badge-error badge-lg'>
+            <InfoIcon className='mr-2' /> {open_issues}
           </div>
-          <div className="mr-2 badge-warning badge-lg">
-            {<RestaurantIcon className="mr-2" />}
-            {forks}
+          <div className='mr-2 badge badge-warning badge-lg'>
+            <RestaurantIcon className='mr-2' /> {forks}
           </div>
         </div>
       </div>
