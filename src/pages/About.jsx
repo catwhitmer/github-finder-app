71a1970af4f29c137a614e0ad8b6047cca0f8a4b
Cat Whitmer <60889037+catwhitmer@users.noreply.github.com>
2022-11-20 11:30:19 -0500

    Updated About page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -6,26 +6,7 @@ function About() {
         A React app to search GitHub profiles and see profile details.
       </p>
       <p className="text-lg text-gray-400">
-        Version <span className="text-white">1.0.0</span>
-      </p>
-      <p className="text-lg text-gray-400">
-        App was created with the help of:
-        <a
-          className="text-white"
-          href="https://www.udemy.com/course/modern-react-front-to-back/"
-        >
-          React Front To Back
-          <a className="text-white" href="https://traversymedia.com">
-            {" "}
-            by Brad Traversy
-          </a>
-        </a>
-      </p>
-      <p className="text-lg text-gray-400">
-        Layout inspired By:
-        <a className="text-white" href="https://twitter.com/hassibmoddasser">
-          Hassib Moddasser
-        </a>
+        Version <span className="text-white">1.0.2</span>
       </p>
     </>
   );
